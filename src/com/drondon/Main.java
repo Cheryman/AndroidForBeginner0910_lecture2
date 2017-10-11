6dc7dcc7f836af331d32a5503c46b6ce2e29297f
Andrii Miroshnychenko <collosteam@gmail.com>
2017-10-11 20:57:11 +0300

    Ternar operators

--- src/com/drondon/Main.java
+++ src/com/drondon/Main.java
@@ -6,32 +6,14 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         while (true) {
-            System.out.println("Введи почту!!!!:");
-            String result = scanner.nextLine();
-
+            System.out.println("Введи число!!!!:");
+            int result = scanner.nextInt();
             if ("exit".equals(result)) {
                 System.out.println("Пока!");
                 return;
             }
-
-            System.out.println("Введи пароль!!!!:");
-            String resultPassword = scanner.nextLine();
-
-            int indexOfAt = result.indexOf("@");
-            boolean isEmail = indexOfAt >= 0;
-
-            if (!isEmail && indexOfAt > 0) {
-                System.out.println("IS TEXT");
-            } else if (resultPassword.length() < 6) {
-                System.out.println("PASSWORD MUST BE 6 OR MORE LETTERS!");
-            } else {
-                System.out.println("IS EMAIL");
-            }
-
+            System.out.println("Число : " + ((result % 2 == 0) ? "четное" : "нечетное"));
         }
-
     }
-
 }
