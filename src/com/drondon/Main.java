ec4f77ef5d09a6773f7c8a7c3b0ed888f5c5b5c8
Andrii Miroshnychenko <collosteam@gmail.com>
2017-10-11 19:48:56 +0300

    Email validator

--- src/com/drondon/Main.java
+++ src/com/drondon/Main.java
@@ -1,22 +1,27 @@
 package com.drondon;
 
+import java.util.Scanner;
+
 public class Main {
 
-    //My first comment
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Введи почту!!!!:");
+            String result = scanner.nextLine();
+            if ("exit".equals(result)) {
+                System.out.println("Пока!");
+                return;
+            }
+
+            int indexOfAt = result.indexOf("@");
+            boolean isEmail = indexOfAt >= 0;
+
+            System.out.println("Is email: " + isEmail);
+
+        }
+
     }
 
-    /* My first multi
-      lines comment*/
-    public static final String test(){
-        return "";
-    }
-
-    /**
-     * My documented comment
-     * */
-    public static final void printDoc(){
-
-    }
 }
