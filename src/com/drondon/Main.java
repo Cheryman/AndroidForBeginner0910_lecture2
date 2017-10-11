09c69f0d36d15ab89d39ba3cb1ce37017e39150e
Andrii Miroshnychenko <collosteam@gmail.com>
2017-10-11 19:28:50 +0300

    Comments

---
+++ src/com/drondon/Main.java
@@ -0,0 +1,22 @@
+package com.drondon;
+
+public class Main {
+
+    //My first comment
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    /* My first multi
+      lines comment*/
+    public static final String test(){
+        return "";
+    }
+
+    /**
+     * My documented comment
+     * */
+    public static final void printDoc(){
+
+    }
+}
