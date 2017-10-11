9fa367b666a7903957b92637a6897279dd79690f
Andrii Miroshnychenko <collosteam@gmail.com>
2017-10-11 20:41:21 +0300

    If else

--- src/com/drondon/Main.java
+++ src/com/drondon/Main.java
@@ -4,10 +4,34 @@ import java.util.Scanner;
 
 public class Main {
 
-    static long a = 1L;
-
     public static void main(String[] args) {
-        int value = (int) a;
-        int result = (int) (1232213232L * 1233312321);
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Введи почту!!!!:");
+            String result = scanner.nextLine();
+
+            if ("exit".equals(result)) {
+                System.out.println("Пока!");
+                return;
+            }
+
+            System.out.println("Введи пароль!!!!:");
+            String resultPassword = scanner.nextLine();
+
+            int indexOfAt = result.indexOf("@");
+            boolean isEmail = indexOfAt >= 0;
+
+            if (!isEmail && indexOfAt > 0) {
+                System.out.println("IS TEXT");
+            } else if (resultPassword.length() < 6) {
+                System.out.println("PASSWORD MUST BE 6 OR MORE LETTERS!");
+            } else {
+                System.out.println("IS EMAIL");
+            }
+
+        }
+
     }
+
 }
