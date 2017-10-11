96612b56da3e0c58f02fec1e9f65d05c5db88304
Andrii Miroshnychenko <collosteam@gmail.com>
2017-10-11 20:12:28 +0300

    Added boolean parser

--- src/com/drondon/Main.java
+++ src/com/drondon/Main.java
@@ -4,24 +4,20 @@ import java.util.Scanner;
 
 public class Main {
 
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.println("Введи почту!!!!:");
+            System.out.println("Введи true или false!!!!:");
             String result = scanner.nextLine();
             if ("exit".equals(result)) {
                 System.out.println("Пока!");
                 return;
             }
 
-            int indexOfAt = result.indexOf("@");
-            boolean isEmail = indexOfAt >= 0;
-
-            System.out.println("Is email: " + isEmail);
-
+            boolean value = Boolean.valueOf(result);
+            System.out.println("Value: " + value);
         }
-
     }
-
 }
